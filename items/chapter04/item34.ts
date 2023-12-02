e558578a1146936a02fbbf49158fd40f4b9f27cd
takuto-yamamoto <tyguitar0v0627@gmail.com>
2023-12-02 16:32:17 +0900

    item34: Prefer incomplete types to inaccurate types

---
+++ items/chapter04/item34.ts
@@ -0,0 +1,13 @@
+/**
+ * Item34: Prefer incomplete types to inaccurate types
+ *
+ * Things to remember
+ * - avoid uncanny valley of type safety:
+ * - incorrect types are often worse than no types
+ *
+ * - if you cannot model a type accurately, do not model it inaccurately
+ * - Acknowledge tha gaps using any or unknown
+ *
+ * - pay attention to error messages and autocomplete as you make typings increasingly precise
+ * - it's not just about correctness: developer experience matters, too
+ */
