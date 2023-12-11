ef954157ada561dfd8fde806146c998d3d735f6a
takuto-yamamoto <tyguitar0v0627@gmail.com>
2023-12-12 00:10:07 +0900

    Item 46: Understand the Three Versions Involved in Type Declarations

---
+++ items/chapter06/item46.ts
@@ -0,0 +1,11 @@
+/**
+ * Item 46: Understand the Three Versions Involved in Type Declarations
+ *
+ * Things to remember
+ * - There are three versions involved in an @types dependency: the library version, the @types version, and the TypeScript version.
+ *
+ * - If you update a library, make sure you update the corresponding @types
+ *
+ * - Understand the pros and cons of bundling types versus publishing them on DefinitelyTyped.
+ * - Prefer bundling types if your library is written in TypeScript and DefinitelyTyped if it is not.
+ */
