a0a8aeb9134271d1c27fff1516472268291f25e4
takuto-yamamoto <tyguitar0v0627@gmail.com>
2023-12-13 01:10:43 +0900

    Item 56: Don’t Rely on Private to Hide Information

---
+++ items/chapter07/item56.ts
@@ -0,0 +1,10 @@
+/**
+ * Item 56: Don’t Rely on Private to Hide Information
+ *
+ * Things to remember
+ * - The private access modifier is only enforced through the type system.
+ * - It has no effect at runtime and can be bypassed with an assertion.
+ * - Don’t assume it will keep data hidden.
+ *
+ * - For more reliable information hiding, use a closure or private field
+ */
