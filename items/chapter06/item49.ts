09eea3f1f423d4b402652b530095b03f78a80e6a
takuto-yamamoto <tyguitar0v0627@gmail.com>
2023-12-12 23:21:10 +0900

    Item 49: Provide a Type for this in Callbacks

---
+++ items/chapter06/item49.ts
@@ -0,0 +1,8 @@
+/**
+ * Item 49: Provide a Type for this in Callbacks
+ *
+ * Things to remember
+ * - understand how this binding works
+ *
+ * - provide a type for this in callback when it's part of your API
+ */
