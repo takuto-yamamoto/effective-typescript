7e028f827a22cd13ae178a261947a4792c84f21e
takuto-yamamoto <tyguitar0v0627@gmail.com>
2023-12-13 01:07:33 +0900

    Item 55: Understand the DOM hierarchy

---
+++ items/chapter07/item55.ts
@@ -0,0 +1,12 @@
+/**
+ * Item 55: Understand the DOM hierarchy
+ *
+ * Things to remember
+ * - The DOM has a type hierarchy that you can usually ignore while writing JavaScript.
+ * - But these types become more important in TypeScript.
+ * - Understanding them will help you write TypeScript for the browser.
+ *
+ * - Know the differences between Node, Element, HTMLElement, and EventTarget, as well as those between Event and MouseEvent.
+ *
+ * - Either use a specific enough type for DOM elements and Events in your code or give TypeScript the context to infer it.
+ */
