2fc0816bf765770dab2edc65d33087ec42d31787
takuto-yamamoto <tyguitar0v0627@gmail.com>
2023-12-13 20:03:50 +0900

    Item60:  Use allowJs to Mix TypeScript and JavaScript

---
+++ items/chapter08/item60.ts
@@ -0,0 +1,8 @@
+/**
+ * Item60:  Use allowJs to Mix TypeScript and JavaScript
+ *
+ * Things to remember
+ * - Use the allowJs compiler option to support mixed JavaScript and TypeScript as you transition your project.
+ *
+ * - Get your tests and build chain working with TypeScript before beginning largeScale migration
+ */
