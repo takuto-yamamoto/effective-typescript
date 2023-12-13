b11b450f354e6490f959d00d42479a6f9d5748d0
takuto-yamamoto <tyguitar0v0627@gmail.com>
2023-12-13 19:46:06 +0900

    Item58: Write Modern JavaScript

---
+++ items/chapter08/item58.ts
@@ -0,0 +1,23 @@
+/**
+ * Item58: Write Modern JavaScript
+ *
+ * Things to remember
+ * - TypeScript lets you write modern JavaScript whatever your runtime environment.
+ * - Take advantage of this by using the language features it enables.
+ * - In addition to improving your codebase, this will help TypeScript understand your code.
+ *
+ * - Use TypeScript to learn language features like classes, destructuring, and async/await
+ *
+ * - Don’t bother with 'use strict': TypeScript is stricter
+ *
+ * - Check the TC39 GitHub repo and TypeScript release notes to learn about all the latest language features.
+ */
+
+// ESM >> CommonJS
+// Classes >> Prototypes
+// let/const >> var
+// for-of >> for(;;)
+// Arrow Functions >> function expressions
+// Use Compact Object Literals and Destructuring Assignment
+// Use Default Function Parameters
+// async/await >> Raw Promises or Callbacks
