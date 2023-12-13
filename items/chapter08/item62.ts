8a8d146b6ba90fd21e048a9fd3a8941aa8d40d3f
takuto-yamamoto <tyguitar0v0627@gmail.com>
2023-12-13 20:11:26 +0900

    Item 62: Don’t Consider Migration Complete Until You Enable noImplicitAny

---
+++ items/chapter08/item62.ts
@@ -0,0 +1,10 @@
+/**
+ * Item 62: Don’t Consider Migration Complete Until You Enable noImplicitAny
+ *
+ * Things to remember
+ * - Don’t consider your TypeScript migration done until you adopt noImplicitAny.
+ * - Loose type checking can mask real mistakes in type declarations
+ *
+ * - Fix type errors gradually before enforcing noImplicitAny.
+ * - Give your team a chance to get comfortable with TypeScript before adopting stricter checks.
+ */
