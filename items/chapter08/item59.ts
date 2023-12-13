31c6bb3c4de513b863d2168db4788299f902f3d4
takuto-yamamoto <tyguitar0v0627@gmail.com>
2023-12-13 19:59:02 +0900

    Item 59: Use @ts-check and JSDoc to Experiment with TypeScript

---
+++ items/chapter08/item59.ts
@@ -0,0 +1,13 @@
+/**
+ * Item 59: Use @ts-check and JSDoc to Experiment with TypeScript
+ *
+ * Things to remember
+ * - Add @ts-check to the top of a JS file to enable type checking
+ *
+ * - recognize common errors. know how to declare globals and add type declarations for third-party libraries
+ *
+ * - use JSDoc annotations for type assertions and better type inference
+ *
+ * - don't spend too much time getting your code perfectly typed with JSDoc
+ * - remember that the goal is to convert to .ts
+ */
